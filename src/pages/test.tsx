d955abe9a3213536383cb5490f55d427d049c40b
Hugh <ygz14835187@163.com>
2023-02-24 17:40:57 +0800

    change test

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -2,36 +2,10 @@ import { Button } from '@nextui-org/react';
 // import { MtvDb } from '@/lib/mtv-db';
 import { ethers } from 'ethers';
 import EthCrypto from 'eth-crypto';
-import * as ethUtil from 'ethereumjs-util';
 export default function Test() {
   const start = async () => {
     // const privateKey =
     //   '8bf625a828253a687b6d61b81736b42fa5f38b24b48c7efdbae173c53c656f2b';
-    const signHash = ethers.solidityPackedKeccak256(
-      ['address', 'string'],
-      [
-        '0x5c821001A235D51b09b73D5fB009e30A474C5388',
-        '0x5c821001A235D51b09b73D5fB009e30A474C5388',
-      ],
-    );
-    console.log(signHash);
-    const privateKey =
-      '0x78831134a715e9a75a09d0066dc0ed69f0422a778f1acb28623c1398820f8c9b';
-    const signMessage = EthCrypto.sign(privateKey, signHash);
-    console.log(signMessage);
-
-    var message = ethUtil.toBuffer(
-      '0x5c821001A235D51b09b73D5fB009e30A474C5388',
-    );
-    var msgHash = ethUtil.hashPersonalMessage(message);
-
-    var privateHexKey = new Buffer(
-      '78831134a715e9a75a09d0066dc0ed69f0422a778f1acb28623c1398820f8c9b',
-      'hex',
-    );
-    var sig = ethUtil.ecsign(msgHash, privateHexKey);
-    console.log(sig);
-    // const signer = new ethers.Singer()
     // const mtvdb = new MtvDb();
     // await mtvdb.createInstance(privateKey, '');
     // const value001 = {
