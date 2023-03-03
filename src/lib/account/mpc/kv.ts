a7dc33400ad080adfb969ab381fe1dd943df4107
wangqingfeng <wangqingfeng@tinyverse.space>
2023-03-03 18:07:45 +0800

    fixed bug

---
+++ src/lib/account/mpc/kv.ts
@@ -0,0 +1,115 @@
+import CryptoJS from 'crypto-js';
+import axios from 'axios';
+import { Logger } from 'tslog';
+import { config } from './config';
+
+const logger = new Logger({ name: 'KeySha' });
+
+export class KeySha {
+  
+  public async get(userId:string, question:string, answer:string){
+    const aesSupplyInfo = this.generateAesSupple(userId, question, answer);
+    const dataKey = aesSupplyInfo.key;
+    const encryptShareKey = await this.getKeyFromKvServer(dataKey);
+    const shareKey = this.aesDecode(aesSupplyInfo.aes_key, aesSupplyInfo.aes_iv, encryptShareKey)
+    return shareKey;
+  }
+
+  public async set(userId:string, question:string, answer:string, shareKey: string){
+    const aesSupplyInfo = this.generateAesSupple(userId, question, answer);
+    const encryptShareKey = this.aesEncode(aesSupplyInfo.aes_key, aesSupplyInfo.aes_iv, shareKey);
+    const dataKey = aesSupplyInfo.key;
+    return await this.setKeyToKvServer(dataKey, encryptShareKey);
+  }
+
+  private generateAesSupple(userId:string, question:string, answer:string) :any {
+    const concatStr = config.kv.qasks_api_key + userId + question + answer;
+    const key = CryptoJS.SHA3(concatStr).toString();
+    const aesIv = CryptoJS.enc.Utf8.parse(answer);
+    const aesKey = CryptoJS.enc.Utf8.parse(answer);
+    return {
+      'key': key,
+      'aes_iv': aesIv,
+      'aes_key': aesKey
+    }
+  }
+  
+  private async getKeyFromKvServer(key: string) {
+    const httpConfig = {
+      headers: {
+        qasks_api_key: config.kv.qasks_api_key,
+        qasks_api_secret_key: config.kv.qasks_api_secret_key,
+      },
+    };
+    const getKeyUrl =
+      config.kv.key_server_url + config.kv.get_key_url + key;
+    return axios
+      .get(getKeyUrl, httpConfig)
+      .then((res) => {
+        if(res.data.code  == '000000' ){
+          logger.info(res.data.data);
+          logger.info("get key:" + res.data.data);
+          return res.data.data;
+        }
+        throw new Error(res.data.msg);
+      })
+      .catch((err) => {
+        logger.error(err);
+        throw err;
+      });
+  }
+
+  private async setKeyToKvServer(key: string, value: string) {
+    const httpConfig = {
+      headers: {
+        'qasks_api_key': config.kv.qasks_api_key,
+        'qasks_api_secret_key': config.kv.qasks_api_secret_key
+      },
+    };
+    const setKeyUrl = config.kv.key_server_url + config.kv.set_key_url
+    const setData = {
+      "key": key,
+      "value": value
+    }
+    return axios
+      .post(setKeyUrl, setData, httpConfig)
+      .then((res) => {
+        if(res.data.code  == '000000' ){
+          logger.info("set key:" + res.data.data);
+          return res.data.data;
+        }
+        logger.error(res.data.msg);
+        throw new Error(res.data.msg);
+      })
+      .catch((err) => {
+        logger.error(err);
+        throw err;
+      });
+  }
+
+  aesEncode(aesKey:any, aesIv:any, data: string) {
+    let srcs = CryptoJS.enc.Utf8.parse(data);
+    let encrypted = CryptoJS.AES.encrypt(srcs, aesKey, {
+      iv: aesIv,
+      mode: CryptoJS.mode.CBC,
+      padding: CryptoJS.pad.Pkcs7,
+    });
+    let encryptedBase64Data = CryptoJS.enc.Base64.stringify(
+      encrypted.ciphertext,
+    );
+    return encodeURIComponent(encryptedBase64Data);
+  }
+
+  aesDecode(aesKey:any, aesIv:any, cryptData: string) {
+    cryptData = decodeURIComponent(cryptData);
+    let encryptedHexStr = CryptoJS.enc.Base64.parse(cryptData);
+    let srcs = CryptoJS.enc.Base64.stringify(encryptedHexStr);
+    let decrypt = CryptoJS.AES.decrypt(srcs, aesKey, {
+      iv: aesIv,
+      mode: CryptoJS.mode.CBC,
+      padding: CryptoJS.pad.Pkcs7,
+    });
+    let decryptedStr = decrypt.toString(CryptoJS.enc.Utf8);
+    return decryptedStr.toString();
+  }
+}
--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -2,6 +2,7 @@ import { Button } from '@nextui-org/react';
  import { MtvDb } from '@/lib/mtv-db';
  import { keys, PrivateKey, PublicKey, aes } from 'libp2p-crypto';
  import  CryptoJS from 'crypto-js'
+ import { KeySha } from '@/lib/account/mpc/kv';
 // import { ethers } from 'ethers';
 // import EthCrypto from 'eth-crypto';
 export default function Test() {
@@ -10,37 +11,37 @@ export default function Test() {
     const privateKey = '5f39cceb470cbc0b9259a0099a5238f3cc069858f8275802c17b37bf545d6df2';
     const dbAddress = '/orbitdb/zdpuAmbXnVrBYvjhUCXG6hTzKHUiNug8KvvXzPnaoZYBV6bQT/mtv_kv';
     const metadataKey = 'kzwfwjn5ji4pupd2pb0qcfwlah3qc9ud9w2n1d3vgesiiidv1uk7t1wfx5ntb6w';
-    const mtvdb = MtvDb.getInstance();
-    //await mtvdb.createInstance(privateKey, dbAddress, metadataKey);
-    //第一次使用,可以拿到一个dbAddress与metadataKey通过下面接口获取
-    //mtvdb.dbAddress
-    //mtvdb.metadataKey
-    //await mtvdb.createInstance(privateKey);
+    // const mtvdb = MtvDb.getInstance();
+    // //await mtvdb.createInstance(privateKey, dbAddress, metadataKey);
+    // //第一次使用,可以拿到一个dbAddress与metadataKey通过下面接口获取
+    // //mtvdb.dbAddress
+    // //mtvdb.metadataKey
+    // //await mtvdb.createInstance(privateKey);
 
-    //后续正常使用（）
-    await mtvdb.createInstance(privateKey, dbAddress, metadataKey);
+    // //后续正常使用（）
+    // await mtvdb.createInstance(privateKey, dbAddress, metadataKey);
   
-    //完全恢复（）
-    //await mtvdb.createInstance(privateKey, '', metadataKey);
+    // //完全恢复（）
+    // //await mtvdb.createInstance(privateKey, '', metadataKey);
 
-    //数据的增改
-    const value001 = {
-      personalInfo: { myname: 'jack', wehight: '120' }
-    };
+    // //数据的增改
+    // const value001 = {
+    //   personalInfo: { myname: 'jack', wehight: '120' }
+    // };
 
-    const value002 = {
-      'is': 'bob'
-    }
-    await mtvdb.put('k001', JSON.stringify(value001));
-    await mtvdb.put('k002', JSON.stringify(value002));
-    await mtvdb.put('k003', JSON.stringify(value002));
-    await mtvdb.put('k004', JSON.stringify(value002));
-    await mtvdb.put('k005', JSON.stringify(value002));
-    await mtvdb.put('k006', JSON.stringify(value002));
-    const get1 = await mtvdb.get('k001');
-    const get2 = await mtvdb.get('k002');
-    const get3 = await mtvdb.get('k002');
-    await mtvdb.backupDb();
+    // const value002 = {
+    //   'is': 'bob'
+    // }
+    // await mtvdb.put('k001', JSON.stringify(value001));
+    // await mtvdb.put('k002', JSON.stringify(value002));
+    // await mtvdb.put('k003', JSON.stringify(value002));
+    // await mtvdb.put('k004', JSON.stringify(value002));
+    // await mtvdb.put('k005', JSON.stringify(value002));
+    // await mtvdb.put('k006', JSON.stringify(value002));
+    // const get1 = await mtvdb.get('k001');
+    // const get2 = await mtvdb.get('k002');
+    // const get3 = await mtvdb.get('k002');
+    // await mtvdb.backupDb();
     //await mtvdb.closeDb();
     // const aesIv =  CryptoJS.enc.Utf8.parse(privateKey.slice(-16));
     // const aesKey = CryptoJS.enc.Utf8.parse(privateKey);
@@ -54,6 +55,15 @@ export default function Test() {
     // let decrypt = CryptoJS.AES.decrypt(srcsBase64, aesKey, { iv: aesIv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7 });
     // let decryptedStr = decrypt.toString(CryptoJS.enc.Utf8);
     // console.log(decryptedStr.toString());
+
+    const ks = new KeySha();
+    const userId = "jackcn2004@hotmail.com";
+    const question = "你的身份证号是多少？";
+    const answeer = "430422198609231666";
+    const shareKey = privateKey;
+    const set = await ks.set(userId, question, answeer, shareKey);
+    const get = await ks.get(userId, question, answeer);
+
   };
 
   return <Button onPress={start}>测试</Button>;
