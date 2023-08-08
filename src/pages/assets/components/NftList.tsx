c9ea71a7cd409a46cd071150fbd7d17f776f34c5
Hugh <ygz14835187@163.com>
2023-08-08 15:22:08 +0800

    去掉默认nft

--- src/pages/assets/components/NftList.tsx
+++ src/pages/assets/components/NftList.tsx
@@ -3,16 +3,16 @@ import { useEffect, useState } from 'react';
 import { AssetsNftItem } from './AssetsNftItem';
 export const NftList = () => {
   const [list, setList] = useState<any[]>([
-    {
-      Nftname:
-        '659f0ecbdfcad86b50c02176f978836ab33707d512f7855fab7e840974675cf5',
-      Name: 'IMG_20230801_175247.PNG',
-      DataType: 'image/png',
-      Cid: 'QmfUM8m4sZrRxwkdYEzL29Yj8UwSCmyx1ZQ5TnkPFfnKe6',
-      Data: null,
-      Owner: 'CAESIOhjkWkNhpuTxoCjwmVl8dALULDJDrhP9r6ZdqVj4dKO',
-      Description: 'IMG_20230801_175247.PNG',
-    },
+    // {
+    //   Nftname:
+    //     '659f0ecbdfcad86b50c02176f978836ab33707d512f7855fab7e840974675cf5',
+    //   Name: 'IMG_20230801_175247.PNG',
+    //   DataType: 'image/png',
+    //   Cid: 'QmfUM8m4sZrRxwkdYEzL29Yj8UwSCmyx1ZQ5TnkPFfnKe6',
+    //   Data: null,
+    //   Owner: 'CAESIOhjkWkNhpuTxoCjwmVl8dALULDJDrhP9r6ZdqVj4dKO',
+    //   Description: 'IMG_20230801_175247.PNG',
+    // },
   ]);
   const getList = async () => {
     const { data, code } = await account.getNftList();
