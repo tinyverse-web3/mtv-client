5191d26fcd2f4567d4745e91c753f66de6927e7b
Hugh <ygz14835187@163.com>
2023-08-30 13:51:36 +0800

    默认密码提示信息增加翻译

--- src/components/PasswordWarnBadge.tsx
+++ src/components/PasswordWarnBadge.tsx
@@ -1,19 +1,22 @@
 import { Badge } from '@nextui-org/react';
 import { useNavigate } from 'react-router-dom';
 import { ROUTE_PATH } from '@/router';
-import { useAccountStore } from '@/store'
+import { useAccountStore } from '@/store';
+import { useTranslation } from 'react-i18next';
 
 export const PasswordWarnBadge = () => {
   const { accountInfo } = useAccountStore((state) => state);
   const nav = useNavigate();
-
+  const { t } = useTranslation();
   const clickHandler = async () => {
     nav(ROUTE_PATH.ACCOUNT_CHANGE_PWD);
   };
-  return accountInfo.isDefaultPwd ? (
-    <Badge color='error' className='cursor-pointer' onClick={clickHandler}>
-      请尽快修改本地默认密码
-    </Badge>
+  return !!accountInfo.isDefaultPwd ? (
+    <div className='flex items-center justify-center mb-2'>
+      <Badge color='error' className='cursor-pointer' onClick={clickHandler}>
+        {t('common.password.change_default_hint')}
+      </Badge>
+    </div>
   ) : (
     <></>
   );
