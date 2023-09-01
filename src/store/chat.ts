b4a0670833f15310fe4bcfa919972ae662b17860
Hugh <ygz14835187@163.com>
2023-09-01 15:46:38 +0800

    add version

--- src/store/chat.ts
+++ src/store/chat.ts
@@ -34,28 +34,36 @@ interface ChatState {
 }
 
 export const useChatStore = create<ChatState>()(
-  devtools((set, get) => ({
-    contacts: [],
-    recipient: undefined,
-    remove: async (MessageKey) => {
-      const list = cloneDeep(get().contacts);
-      remove(list, (i) => i.MessageKey === MessageKey);
-      set({ contacts: list });
-    },
-    getContacts: async () => {
-      const list = await account.getContacts();
-      if (get().contacts.length !== list.length) {
-        set({ contacts: list });
-      }
-    },
-    setRecipient: async (n) => {
-      set({ recipient: n });
-    },
-    reset: () => {
-      set({
+  devtools(
+    persist(
+      (set, get) => ({
         contacts: [],
         recipient: undefined,
-      });
-    },
-  })),
+        remove: async (MessageKey) => {
+          const list = cloneDeep(get().contacts);
+          remove(list, (i) => i.MessageKey === MessageKey);
+          set({ contacts: list });
+        },
+        getContacts: async () => {
+          const list = await account.getContacts();
+          if (get().contacts.length !== list.length) {
+            set({ contacts: list });
+          }
+        },
+        setRecipient: async (n) => {
+          set({ recipient: n });
+        },
+
+        reset: () => {
+          set({
+            contacts: [],
+            recipient: undefined,
+          });
+        },
+      }),
+      {
+        name: 'chat-storage',
+      },
+    ),
+  ),
 );
