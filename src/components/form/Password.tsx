71d0e4827bba87b1291844536c5139d0aef50461
Hugh <ygz14835187@163.com>
2023-10-07 14:26:54 +0800

    fixed bug: 密码输入的问题

--- src/components/form/Password.tsx
+++ src/components/form/Password.tsx
@@ -1,12 +1,20 @@
 import { Input as NextInput } from '@nextui-org/react';
+import { divide } from 'lodash';
+import { Icon } from '@iconify/react';
 import { useEffect, useRef, useState } from 'react';
 
 interface Props {
   value?: string | number | readonly string[] | undefined;
   onChange?: (text: string) => void;
 }
-export const Password = ({ value, onChange, ...rest }: Props & any) => {
-
+export const Password = ({
+  value,
+  onChange,
+  isClearable,
+  ...rest
+}: Props & any) => {
+  const [show, setShow] = useState(false);
+  const handleClick = () => setShow(!show);
   const inputRef = useRef<HTMLInputElement | null>(null);
   const chanageSuccess = (val: string) => {
     onChange && onChange(val);
@@ -16,6 +24,7 @@ export const Password = ({ value, onChange, ...rest }: Props & any) => {
   };
   const clearHandler = () => {
     chanageSuccess('');
+    setShow(false);
   };
   useEffect(() => {
     if (inputRef.current) {
@@ -27,13 +36,30 @@ export const Password = ({ value, onChange, ...rest }: Props & any) => {
       aria-label='Password'
       variant='bordered'
       {...rest}
-      isClearable
-      type='password'
+      isClearable={false}
+      type={show ? 'text' : 'password'}
       ref={inputRef}
+      endContent={
+        value ? (
+          <div className='cursor-pointer flex items-center'>
+            <Icon
+              icon={show ? 'eva:eye-off-outline' : 'eva:eye-outline'}
+              className='text-2xl'
+              onClick={handleClick}
+            />
+            <Icon
+              icon='pajamas:clear'
+              className='text-md text-gray-500 ml-1'
+              onClick={clearHandler}
+            />
+          </div>
+        ) : (
+          <></>
+        )
+      }
       // defaultValue={value}
       // initialValue={value}
       onChange={changeHandler}
-      onClear={clearHandler}
     />
   );
 };
