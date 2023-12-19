98045c5ab825ca8d5029a5da1fb014127529c469
Hugh <ygz14835187@163.com>
2023-12-19 17:23:23 +0800

    change telegram bot

--- src/components/OauthTelegram.tsx
+++ src/components/OauthTelegram.tsx
@@ -13,7 +13,7 @@ export const OauthTelegram = ({ onChange }: OauthTelegramProps) => {
   return (
     <div className='flex justify-center relative overflow-hidden'>
       {/* <Image src='/icon-telegram.png' className='w-10 h-10' alt='google' /> */}
-      <TelegramLoginButton botName='ItToolBot' dataOnauth={onTelegramAuth} buttonSize='medium'/>
+      <TelegramLoginButton botName='pay2view_bot' dataOnauth={onTelegramAuth} buttonSize='medium'/>
     </div>
   );
 };
